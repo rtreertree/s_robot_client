244f3051345a11e5d4835ce4a942db59c648cb05
Rtree <88918844+rtreertree@users.noreply.github.com>
2024-09-17 20:01:28 +0700

    a

--- motorcontroller.py
+++ motorcontroller.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
+IRs = [2, 3, 4, 17, 27]
 
 # ir sensor
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.IN)
+GPIO.setup(IRs, GPIO.IN)
 
+currentSensor = [0, 0, 0, 0, 0]
 
 while True:
-    if GPIO.input(2):
-        print("Obstacle detected")
-    else:
-        print("No obstacle detected")
-    time.sleep(1)
+    for i in range(5):
+        currentSensor[i] = GPIO.input(IRs[i])
+    print(currentSensor)
+
+    time.sleep(0.5)
