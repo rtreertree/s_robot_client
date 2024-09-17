8db251532f970be0251194b056ebc44ed6895f5f
Rtree <88918844+rtreertree@users.noreply.github.com>
2024-09-17 19:01:55 +0700

    motor controller

--- motorcontroller.py
+++ motorcontroller.py
@@ -1,30 +1,10 @@
 import gpiozero
 import time
 
-class MotorController:
-    def __init__(self):
-        self.motor = gpiozero.Motor(20, 16)
 
-    def forward(self):
-        self.motor.forward()
-
-    def backward(self):
-        self.motor.backward()
-
-    def stop(self):
-        self.motor.stop()
-
-
-motor = MotorController()
+motor = gpiozero.Motor(20, 16)
 motor.forward()
-
 time.sleep(2)
-
-motor.backward()
-
-time.sleep(2)
-
 motor.stop()
-
-# free resources
-motor.motor.close()
+time.sleep(2)
+motor.close()
