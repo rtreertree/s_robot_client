8cd8e17b46a0babc3aec8bb1fb963b7a03aa6504
Rtree <88918844+rtreertree@users.noreply.github.com>
2024-09-17 20:18:28 +0700

    Add motor

--- motorcontroller.py
+++ motorcontroller.py
@@ -9,9 +9,25 @@ GPIO.setup(IRs, GPIO.IN)
 
 currentSensor = [0, 0, 0, 0, 0]
 
-while True:
-    for i in range(5):
-        currentSensor[i] = GPIO.input(IRs[i])
-    print(currentSensor)
+# while True:
+#     for i in range(5):
+#         currentSensor[i] = GPIO.input(IRs[i])
+#     print(currentSensor)
 
-    time.sleep(0.5)
+#     time.sleep(0.5)
+
+
+# motor
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
+pwm = GPIO.PWM(26, 1000)
+pwm.start(0)
+
+while True:
+    GPIO.output(20, GPIO.HIGH)
+    GPIO.output(16, GPIO.LOW)
+
+    pwm.ChangeDutyCycle(75)
+    time.sleep(1)
