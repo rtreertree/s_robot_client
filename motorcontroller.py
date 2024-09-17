86b0ae8c69e9eaaf6af9994185d046ab580b2dbd
Rtree <88918844+rtreertree@users.noreply.github.com>
2024-09-17 19:20:52 +0700

    a

--- motorcontroller.py
+++ motorcontroller.py
@@ -1,10 +1,17 @@
-import gpiozero
-import time
+import RPi.GPIO as GPIO
 
 
-motor = gpiozero.Motor(19, 13)
-motor.forward()
-time.sleep(2)
-motor.stop()
-time.sleep(2)
-motor.close()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+
+GPIO.setup(26, GPIO.OUT)
+
+pwm = GPIO.PWM(26, 100)
+
+pwm.start(0)
+
+while True:
+    GPIO.output(19, GPIO.HIGH)
+    GPIO.output(13, GPIO.LOW)
+    pwm.ChangeDutyCycle(50)
