7f656c42a2f3c8e14d260716b9f24965a0f53e03
Rtree <88918844+rtreertree@users.noreply.github.com>
2024-09-18 15:49:18 +0700

    a

--- movementController.py
+++ movementController.py
@@ -1,10 +1,13 @@
 import IR_reader
+import motorcontroller
 
 IRs = [2, 3, 4, 17, 27]
 
 ir_reader = IR_reader.IRSensor(IRs)
 sensor = ir_reader.get_sensor()
 
+motorL = motorcontroller.Motor(26, 19, 13)
+motorR = motorcontroller.Motor(6, 5, 12)
 
 def get_error():
     if (sensor[0] == 1 and sensor[1] == 0):
@@ -25,7 +28,39 @@ def get_error():
         return 3
     elif (sensor[4] == 1):
         return 4
-    
 
-print(sensor)
-print(get_error())
+
+while True:
+    error = get_error()
+    if error == 0:
+        motorL.forward(50)
+        motorR.forward(50)
+    elif error == -4:
+        motorL.forward(50)
+        motorR.forward(0)
+    elif error == 4:
+        motorL.forward(0)
+        motorR.forward(50)
+    elif error == -3:
+        motorL.forward(50)
+        motorR.forward(25)
+    elif error == 3:
+        motorL.forward(25)
+        motorR.forward(50)
+    elif error == -2:
+        motorL.forward(50)
+        motorR.forward(37.5)
+    elif error == 2:
+        motorL.forward(37.5)
+        motorR.forward(50)
+    elif error == -1:
+        motorL.forward(50)
+        motorR.forward(43.75)
+    elif error == 1:
+        motorL.forward(43.75)
+        motorR.forward(50)
+    else:
+        motorL.forward(50)
+        motorR.forward(50)
+
+    sensor = ir_reader.get_sensor()
