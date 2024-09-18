04ca82efecfcb9c5872b0d25619f69d19c6024a1
Rtree <88918844+rtreertree@users.noreply.github.com>
2024-09-18 16:06:32 +0700

    a

--- movementController.py
+++ movementController.py
@@ -56,8 +56,8 @@ while True:
         motorR.forward(BASE - (error * 10))
 
     sensor = ir_reader.get_sensor()
-    print(f"{BASE - (error * 12)}, {BASE + (error * 12)}, Error: {error}, ")
-    time.sleep(0.1)
+    # print(f"{BASE - (error * 12)}, {BASE + (error * 12)}, Error: {error}, ")
+    # time.sleep(0.1)
 
 
     lastError = error
